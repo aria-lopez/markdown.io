12f7a18442d283492c25362bec9c11ed83f8e508
aria-lopez <aria.lopez.dev@gmail.com>
2021-10-15 16:17:48 -0700

    Added styling for header as well as base color scheme

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const NavBar = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: left;
+    border-bottom: 1px solid #9AA0A6;
+    width: 100%;
+`;
+
+const NavSpacer = styled.div`
+    min-width: ${props => props.width || null};
+    min-height: 1px;
+`;
 
 export default function App() {
     return (
-        <>
-            <h1>Hello World</h1>
-        </>
+        <Container>
+            <NavBar>
+                <NavSpacer width={'50px'}/>
+                <h1>Markdown.io</h1>
+            </NavBar>
+        </Container>
     )
 }
